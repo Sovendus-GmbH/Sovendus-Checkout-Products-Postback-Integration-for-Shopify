299730c513fb4f68ba395e2c1fc894234bed887c
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2024-11-27 16:33:52 +0100

    try to decode before using values

--- shopify_checkout_products_postback.js
+++ shopify_checkout_products_postback.js
@@ -1,13 +1,13 @@
-var sovendusIdsByLandingPagePath = {
+const sovendusIdsByLandingPagePath = {
   "/products/your-product-landing-page-path": "YOUR_SOVENDUS_PRODUCT_ID",
 };
 
-var sovReqToken = getSovReqTokenFromUrl();
+const sovReqToken = getSovReqTokenFromUrl();
 if (sovReqToken !== null) {
   saveSovReqTokenToCookie(sovReqToken, getSovReqPathFromUrl(), 2678400); // 31 days
 }
 function getSovReqTokenFromUrl() {
-  var url = new URL(window.location);
+  const url = new URL(window.location);
   return url.searchParams.get("sovReqToken");
 }
 function getSovReqPathFromUrl() {
@@ -28,27 +28,27 @@ function saveSovReqTokenToCookie(sovReqToken, sovReqPath, seconds) {
 
 analytics.subscribe("checkout_completed", () => {
   function loadSovInfoFromCookie(cookieName) {
-    var cookieContent = null;
-    var cookie =
+    let cookieContent = null;
+    const cookie =
       document.cookie.split("; ").find(function (entry) {
         return entry.startsWith(cookieName + "=");
       }) || null;
     if (cookie !== null) {
       cookieContent = cookie.split("=")[1];
     }
-    return cookieContent;
+    return decodeURIComponent(cookieContent);
   }
-  var sovReqToken = loadSovInfoFromCookie("sovReqToken");
+  const sovReqToken = loadSovInfoFromCookie("sovReqToken");
   if (sovReqToken) {
-    var sovReqPath = loadSovInfoFromCookie("sovReqPath");
-    var productId = sovendusIdsByLandingPagePath[sovReqPath];
-    var pixel = document.createElement("img");
+    const sovReqPath = loadSovInfoFromCookie("sovReqPath");
+    const productId = sovendusIdsByLandingPagePath[sovReqPath];
+    const pixel = document.createElement("img");
     pixel.src =
       "https://press-order-api.sovendus.com/ext/" +
       productId +
       "/image?sovReqToken=" +
       sovReqToken;
-    document.getElementsByTagName("body")[0].appendChild(pixel);
+    document.body.appendChild(pixel);
     // remove the cookies
     document.cookie =
       "sovReqToken=;path=/;secure;samesite=strict;expires=Thu, 01 Jan 1970 00:00:00 GMT";
